9e38fb2d165becd814690537850dd1bb9b4016d7
Gee Vivek <GeeVivek@v1021-wn-83-113.campus-dynamic.uwaterloo.ca>
2018-09-15 14:45:36 -0400

    First working files

---
+++ app/src/main/java/com/example/scareapp/MapsActivity.java
@@ -0,0 +1,77 @@
+package com.example.scareapp;
+
+import android.app.AlertDialog;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private Marker currentRate;
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+
+            public void onInfoWindowClick(Marker marker) {
+                if (marker.equals(currentRate)){
+
+                    rateNum = (EditText) findViewById(R.id.txtColor);
+                    //Toast.makeText(getBaseContext(), "Marker is clicked", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                mMap.clear();
+                currentRate = mMap.addMarker(new MarkerOptions().position(latLng).title("Click here to rate"));
+
+            }
+        });
+
+
+
+
+        ;
+
+
+        // Add a marker in Waterloo and move the camera
+        LatLng waterloo = new LatLng(43, -80);
+        float zoomLevel = 10.0f;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(waterloo, zoomLevel));
+    }
+}
